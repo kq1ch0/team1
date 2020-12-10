142529bc3f61025e6790d5db30f3f01b0e4866e1
池田 慈英 <16005@kauth.local>
2020-12-10 09:43:48 +0900

    player修正

--- team1/player.java
+++ team1/player.java
@@ -17,18 +17,21 @@ public class player extends Actor
     int gravityPower =   1; //重力
     int groundY      = 600; //地面のY座標
     int jumpVarMax   =   2; //最大ジャンプ回数
+    int vibrationValue = 2; //プレイヤー振動の量
     //int jumpFlex   =  10;
     
     int spdY         =   0; //プレイヤーのY軸速度
     int isPress      =   0; //ボタン押下フラグ
     int jumpVar      = jumpVarMax; //ジャンプ回数カウンタ
-    int stepFlag     = 0;
+    int stepFlag     = 0;   //プレイヤーのブラー処理フラグ
+    
     static int posX         = 0;
     static int posY         = 0;
     
     public void act() 
     {
         // Add your action code here.
+        //-----ジャンプボタン処理-----
         if (Greenfoot.isKeyDown("space") && isPress==0 && jumpVar>0){
             spdY = jumpPower;
             isPress=1;
@@ -38,24 +41,28 @@ public class player extends Actor
             isPress=0;
         }
         
+        //-----プレイヤー移動-----
         setLocation(getX(),getY()+spdY);
         spdY += gravityPower;
-        if (getY() > groundY){
+        
+        //-----着地フラグ処理-----
+        if (getY() > groundY){ 
             setLocation(getX(),600);
             jumpVar = jumpVarMax;
         }
         
+        //-----プレイヤーに走行中のブラーをかける処理-----
         if (jumpVar == jumpVarMax){
             if (stepFlag == 0){
-                setLocation(getX()+5,getY());
+                setLocation(getX()+vibrationValue,getY());
                 stepFlag = 1;
             }else if (stepFlag == 1){
-                setLocation(getX()-5,getY());
+                setLocation(getX()-vibrationValue,getY());
                 stepFlag = 0;
             }
         }
         
-        
+        //-----player_footに渡す座標の指定-----
         posX = getX();
         posY = getY();
     }
