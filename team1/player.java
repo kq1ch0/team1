209ebfcbb71b81487547b31072b12e6f5235a55e
池田 慈英 <16005@kauth.local>
2020-12-17 10:10:48 +0900

    player修正

--- team1/player.java
+++ team1/player.java
@@ -18,12 +18,16 @@ public class player extends Actor
     int groundY      = 600; //地面のY座標
     int jumpVarMax   =   2; //最大ジャンプ回数
     int vibrationValue = 2; //プレイヤー振動の量
+    int ssminiFlagMax  = 15; //砂埃の頻度
     //int jumpFlex   =  10;
     
     int spdY         =   0; //プレイヤーのY軸速度
     int isPress      =   0; //ボタン押下フラグ
     int jumpVar      = jumpVarMax; //ジャンプ回数カウンタ
     int stepFlag     = 0;   //プレイヤーのブラー処理フラグ
+    
+    static int ssFlag = 0;
+    int ssminiFlag   = ssminiFlagMax;
     
     static int posX         = 0;
     static int posY         = 0;
@@ -33,8 +37,17 @@ public class player extends Actor
     public void act() 
     {
         if (jumpVar == jumpVarMax){
-            getWorld().addObject(new sandstorm(),getX()-180,getY());
-            getWorld().addObject(new sandstorm_mini(),getX()-180,getY());
+            if (ssFlag == 0){
+                getWorld().addObject(new sandstorm(),getX()-180,getY());
+                ssFlag = 1;
+            }
+            ssminiFlag --;
+            if (ssminiFlag <= 0){
+                getWorld().addObject(new sandstorm_mini(),getX()-20,getY()+70);
+                ssminiFlag = ssminiFlagMax;
+            }
+        }else{
+            ssFlag = 0;
         }
         // Add your action code here.
         //-----ジャンプボタン処理-----
--- team1/sandstorm.java
+++ team1/sandstorm.java
@@ -14,6 +14,6 @@ public class sandstorm extends Actor
      */
     public void act() 
     {
-        getWorld().removeObject(this);
+        if (player.ssFlag == 0)getWorld().removeObject(this);
     }    
 }
